96a3440ba8a78d8bafc52edadc97491f95765357
1DontEx1st <github@idontexist.com>
2022-12-14 16:37:26 -0500

    Update Credentials.ps1

--- Credentials.ps1
+++ Credentials.ps1
@@ -1,6 +1,14 @@
 # WARNING: This file should *not* be published, since anyone would be able to log in as you.
 
-## Examples
+# Examples
 
-$kongMe = "https://cb-live.synapse-games.com/api.php?user_id=YOUR_USER_ID&password=YOUR_PASSWORD&message="
-$kongAlt = "https://cb-live.synapse-games.com/api.php?user_id=ALT_USER_ID&password=ALT_PASSWORD&message="
+$kongMe = "https://cb-live.synapse-games.com/api.php?user_id=YOUR_PLAYER_ID&password=YOUR_GAME_AUTH_TOKEN&message="
+$kongAlt = "https://cb-live.synapse-games.com/api.php?user_id=ALT_PLAYER_ID&password=ALT_GAME_AUTH_TOKEN&message="
+
+# Player ID is visible on your player profile (click the hero in the top left to see this)
+# Finding your game auth token is slightly more involved. These directions are for using Google Chrome, but most browsers should be similar in functionality:
+# 1. Open dev console (CTRL-Shift-i)
+# 2. Click on "Console", then click on the Filter text box and enter "authentication" (without the quotes)
+# 3. Login to game via web browser (https://www.kongregate.com/games/Throwdown/animation-throwdown)
+# 4. You should see results below the filter showing your player ID and auth token
+# 5. Plug those in above

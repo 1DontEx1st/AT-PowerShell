a959da004b98a58784e1fe1abb6fcd6ea28c14bf
1DontEx1st <github@idontexist.com>
2022-12-14 16:04:11 -0500

    Create Credentials.ps1

---
+++ Credentials.ps1
@@ -0,0 +1,6 @@
+# WARNING: This file should *not* be published, since anyone would be able to log in as you.
+
+## Examples
+
+$kongMe = "https://cb-live.synapse-games.com/api.php?user_id=YOUR_USER_ID&password=YOUR_PASSWORD&message="
+$kongAlt = "https://cb-live.synapse-games.com/api.php?user_id=ALT_USER_ID&password=ALT_PASSWORD&message="
